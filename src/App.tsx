d2bb2b1a9be9d93c02aac315f5b8f10653d9b5ea
jjung-in <4292in@naver.com>
2025-02-23 22:19:51 +0900

    Fix: React Router Dom 콘솔 warning 해결
    - React Router v7에서 변경될 두 가지 동작에 대한 warning이 콘솔에 출력됨
    - v7_startTransition, v7_relativeSplatPath 속성 명시

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,39 @@
-import './App.css'
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./styles/GlobalStyle";
-import { darkTheme } from "./styles/theme";
-import Layout from "./layouts/Layout";
-import Home from "./pages/Home";
-import Search from "./pages/Search";
-import Details from "./pages/Details";
-import FavoritesPage from "./pages/Favorites";
-import Login from "./pages/Login";
-import Join from "./pages/Join";
-import ScrollToTop from "./components/ScrollToTop";
-import { AuthProvider } from "./context/AuthContext";
+import './App.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/GlobalStyle';
+import { darkTheme } from './styles/theme';
+import Layout from './layouts/Layout';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Details from './pages/Details';
+import FavoritesPage from './pages/Favorites';
+import Login from './pages/Login';
+import Join from './pages/Join';
+import ScrollToTop from './components/ScrollToTop';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <AuthProvider>
       <ThemeProvider theme={darkTheme}>
         <GlobalStyle />
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: false, v7_relativeSplatPath: false }}>
           <ScrollToTop />
           <Routes>
-            <Route element={<Layout />} >
-              <Route path="/" element={<Home />} />
-              <Route path="detail/:id" element={<Details />} />
-              <Route path="search" element={<Search />} />
-              <Route path="favorites" element={<FavoritesPage />} />
-              <Route path="login" element={<Login />} />
-              <Route path="join" element={<Join />} />
+            <Route element={<Layout />}>
+              <Route path='/' element={<Home />} />
+              <Route path='detail/:id' element={<Details />} />
+              <Route path='search' element={<Search />} />
+              <Route path='favorites' element={<FavoritesPage />} />
+              <Route path='login' element={<Login />} />
+              <Route path='join' element={<Join />} />
             </Route>
-          </Routes >
+          </Routes>
         </BrowserRouter>
       </ThemeProvider>
     </AuthProvider>
-  )
+  );
 }
 
 export default App;
